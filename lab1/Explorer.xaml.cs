9ba073ff41a0e8df43944ec881c58bfec7e34125
windinmane@gmail.com <YOUR@EMAIL.com>
2017-11-18 15:03:04 +0200

    new version
    changed from browser to treeview. no text field to choose folder, only shows all available folders

--- lab1/Explorer.xaml.cs
+++ lab1/Explorer.xaml.cs
@@ -1,67 +1,170 @@
 ﻿using System;
-using System.Threading;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using System.Windows.Forms;
-using System.Windows.Navigation;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace lab1
 {
- 
+
     public partial class Explorer : Window
     {
-       
-            public Explorer()
+
+        public Explorer()
         {
             InitializeComponent();
-
-            webBrowser.LoadCompleted += browser_LoadCompleted;
+            this.LoadDirectories();
         }
 
-        private void btnOpen_Click(object sender, EventArgs e) //дозволяє користувачу обрати шлях до папки (Browse)
+
+        public void LoadDirectories()
         {
-            using (System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog() { Description = "Please, select path" }) //відкриває стандартний браузер папок для вибору потрібної папки
+            var drives = DriveInfo.GetDrives();  //get the list of the active system drives
+            foreach (var drive in drives)
             {
-                if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK) //якщо папку було обрано і натиснуто ОК
+                this.treeView.Items.Add(this.GetItem(drive));
+            }
+        }
+
+        private TreeViewItem GetItem(DriveInfo drive)
+        {
+            var item = new TreeViewItem
+            {
+                Header = drive.Name,
+                DataContext = drive,
+                Tag = drive
+            };
+            this.AddDummy(item);  //creates a temporary dummy item inside each TreeViewItem, so the TreeViewItem is collapsible without loading the child dirs and files
+            item.Expanded += new RoutedEventHandler(item_Expanded); // event handler to the expand dir list event (arrow click)
+            return item;
+        }
+
+        public class DummyTreeViewItem : TreeViewItem  //determine whether the item is dummy or not
+        {
+            public DummyTreeViewItem()
+                : base()
+            {
+                base.Header = "Dummy";
+                base.Tag = "Dummy";
+            }
+        }
+
+        // overloaded methods of GetItem to get folders and files
+        private TreeViewItem GetItem(DirectoryInfo directory)
+        {
+            var item = new TreeViewItem
+            {
+                Header = directory.Name,
+                DataContext = directory,
+                Tag = directory
+            };
+            this.AddDummy(item);
+            item.Expanded += new RoutedEventHandler(item_Expanded);
+            return item;
+        }
+
+        private TreeViewItem GetItem(FileInfo file)
+        {
+            var item = new TreeViewItem
+            {
+                Header = file.Name,
+                DataContext = file,
+                Tag = file
+            };
+            return item;
+        }
+
+
+        //methods to process Dummies
+        private void AddDummy(TreeViewItem item)
+        {
+            item.Items.Add(new DummyTreeViewItem());
+        }
+
+        private bool HasDummy(TreeViewItem item)
+        {
+            return item.HasItems && (item.Items.OfType<TreeViewItem>().ToList().FindAll(tvi => tvi is DummyTreeViewItem).Count > 0);
+        }
+
+        private void RemoveDummy(TreeViewItem item)
+        {
+            var dummies = item.Items.OfType<TreeViewItem>().ToList().FindAll(tvi => tvi is DummyTreeViewItem);
+            foreach (var dummy in dummies)
+            {
+                item.Items.Remove(dummy);
+            }
+        }
+
+
+        private void ExploreDirectories(TreeViewItem item)
+        {
+            var directoryInfo = (DirectoryInfo)null;  //get the list of child directories and files
+            if (item.Tag is DriveInfo)
+            {
+                directoryInfo = ((DriveInfo)item.Tag).RootDirectory;
+            }
+            else if (item.Tag is DirectoryInfo)
+            {
+                directoryInfo = (DirectoryInfo)item.Tag;
+            }
+            else if (item.Tag is FileInfo)
+            {
+                directoryInfo = ((FileInfo)item.Tag).Directory;
+            }
+            if (object.ReferenceEquals(directoryInfo, null)) return;
+            foreach (var directory in directoryInfo.GetDirectories())     //TODO: exception with usb/cd drives
+            {
+                var isHidden = (directory.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
+                var isSystem = (directory.Attributes & FileAttributes.System) == FileAttributes.System;
+                if (!isHidden && !isSystem)
                 {
-                    webBrowser.Source = new Uri(fbd.SelectedPath); 
-                    txtPath.Text = fbd.SelectedPath; //присвоює Text елементу txtPath шлях до обраної папки
+                    item.Items.Add(this.GetItem(directory));
                 }
             }
-
         }
-        //Кнопка (OK) відкриває папку в webBrowser по введеному користувасем в txtPath шляху
-        private void btnOpen1_Click(object sender, EventArgs e) 
+
+        private void ExploreFiles(TreeViewItem item)
         {
-            try
+            var directoryInfo = (DirectoryInfo)null;
+            if (item.Tag is DriveInfo)
             {
-                webBrowser.Navigate(new Uri(txtPath.Text));
+                directoryInfo = ((DriveInfo)item.Tag).RootDirectory;
             }
-            catch (System.UriFormatException ex)
+            else if (item.Tag is DirectoryInfo)
             {
+                directoryInfo = (DirectoryInfo)item.Tag;
+            }
+            else if (item.Tag is FileInfo)
+            {
+                directoryInfo = ((FileInfo)item.Tag).Directory;
+            }
+            if (object.ReferenceEquals(directoryInfo, null)) return;
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                var isHidden = (file.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
+                var isSystem = (file.Attributes & FileAttributes.System) == FileAttributes.System;
+                if (!isHidden && !isSystem)
+                {
+                    item.Items.Add(this.GetItem(file));
+                }
             }
         }
 
-        //крок назад\вперед (по історії відкриття папок)
-
-        private void btnBack_Click(object sender, EventArgs e) 
+        void item_Expanded(object sender, RoutedEventArgs e)
         {
-            if (webBrowser.CanGoBack)
-           webBrowser.GoBack();
+            var item = (TreeViewItem)sender;
+            if (this.HasDummy(item))    //check whether current item has a dummy and remove it
+            {
+                this.Cursor = Cursors.Wait;
+                this.RemoveDummy(item);
+                this.ExploreDirectories(item);   //load child directories and files
+                this.ExploreFiles(item);
+                this.Cursor = Cursors.Arrow;
+            }
         }
 
-        private void btnForward_Click(object sender, EventArgs e) 
-        {
-            if (webBrowser.CanGoForward)
-            webBrowser.GoForward();
-        }
-        
-        //коли документ завершив своє завантаження
-        void browser_LoadCompleted(object sender, NavigationEventArgs e) 
-        {
-            Console.WriteLine("The document being navigated to has finished downloading.");
-            btnBack.IsEnabled = webBrowser.CanGoBack; //активує\деактивує кнопки навігації в залежності від того, чи їх можна використати
-            btnForward.IsEnabled = webBrowser.CanGoForward;
-            txtPath.Text = webBrowser.Source.ToString();//виводить шлях до поточної папки в  txtPath
-        }
+       
+
     }
 }
--- lab1/Session.cs
+++ lab1/Session.cs
@@ -8,12 +8,14 @@ namespace lab1
 {
     class Session
     {
+        public int ID;
         public string Username { get; set; }
         public DateTime dateTime { get; set; }
         public string Path { get; set; }
 
-        public Session(string username, DateTime dateTime, string path ) 
+        public Session(int ID, string username, DateTime dateTime, string path ) 
         {
+            this.ID = ID;
             this.Username = username;
             this.dateTime = dateTime;
             this.Path = path;
